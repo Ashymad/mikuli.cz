300a598f0509548243baaece309814b335a58d26
Szymon Mikulicz <szymon.mikulicz@posteo.net>
2026-06-12 23:06:44 +0200

    Adjust for busybox grep

--- generate.sh
+++ generate.sh
@@ -4,11 +4,13 @@
 $PHP yellow.php generate
 
 cd public
-for file in $(grep -Rl '<!DOCTYPE html>' | grep -v '.html$'); do
+for file in $(grep -Rl '<!DOCTYPE html>' . | grep -v '.html$'); do
     echo -n "Fixing $file:"
     mv $file $file.html
-    for file2 in $(grep -Rl 'href="/'"$file"'"'); do
-        sed 's@href="/'"$file"'"@href="/'"$file"'.html"@g' "$file2" > "$file2.tmp"
+    olink='href="'"${file#.}"'"'
+    nlink='href="'"${file#.}"'.html"'
+    for file2 in $(grep -Rl "$olink" .); do
+        sed "s@$olink@$nlink@g" "$file2" > "$file2.tmp"
         mv "$file2.tmp" "$file2"
         echo -n " $file2"
     done
