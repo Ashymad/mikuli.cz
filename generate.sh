84c43fa59a6f8a12b5f9fde2acc24482b8ef1b47
Szymon Mikulicz <szymon.mikulicz@posteo.net>
2026-06-12 22:53:11 +0200

    Make the script verbose

--- generate.sh
+++ generate.sh
@@ -5,10 +5,13 @@ $PHP yellow.php generate
 
 cd public
 for file in $(grep -Rl '<!DOCTYPE html>' | grep -v '.html$'); do
+    echo -n "Fixing $file:"
     mv $file $file.html
     for file2 in $(grep -Rl 'href="/'"$file"'"'); do
         sed 's@href="/'"$file"'"@href="/'"$file"'.html"@g' "$file2" > "$file2.tmp"
         mv "$file2.tmp" "$file2"
+        echo -n " $file2"
     done
+    echo ""
 done
 cd ..
